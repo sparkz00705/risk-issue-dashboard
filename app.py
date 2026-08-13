d742208ba246ae6417f075c07d14530e81f353f3
sparkz00705 <sparkz007@gmail.com>
2026-08-13 18:47:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,14 @@ import plotly.express as px
 from groq import Groq
 import streamlit as st
 import streamlit.components.v1 as components
+import streamlit_gtag as st_gtag
+
+
+# --- 1. PLACE GOOGLE ANALYTICS CODE RIGHT HERE ---
+st_gtag(
+    gtag_id="G-60MBXMF62S",
+    config={"send_page_view": True}
+)
 
 # --- PAGE CONFIGURATION & LUXURY STYLING ---
 st.set_page_config(
