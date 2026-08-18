f6714e21a507243fa020d9e83ab7ce843833ebfb
sparkz00705 <sparkz007@gmail.com>
2026-08-18 17:33:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ dashboard_page = st.Page(
 )
 
 rtm_page = st.Page(
-    page="pages/2_Requirements_Traceability_Matrix.py", 
+    page="pages/rtm_page.py", 
     title="Requirements Traceability Matrix", 
     icon="🔗"
 )
